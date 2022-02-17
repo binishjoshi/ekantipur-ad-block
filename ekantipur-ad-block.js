bd0562464339bbd0043841c74ced439510537e48
cyclonejet <cyelonejet1@protonmail.com>
2022-02-18 05:20:22 +0545

    added block for whole screen ad and added updateURL and downloadURL

--- ekantipur-ad-block.js
+++ ekantipur-ad-block.js
@@ -5,6 +5,8 @@
 // @grant       none
 // @version     0.01
 // @author      neo
+// @updateURL   https://github.com/cyclonejet/ekantipur-ad-block/raw/master/ekantipur-ad-block.js
+// @downloadURL https://github.com/cyclonejet/ekantipur-ad-block/raw/master/ekantipur-ad-block.js
 // @description 2/16/2022, 12:18:28 AM
 // ==/UserScript==
 
@@ -20,3 +22,6 @@ var styles = `
 var styleSheet = document.createElement("style");
 styleSheet.innerText = styles;
 document.head.appendChild(styleSheet);
+
+var roadblock_ad = document.querySelector("#roadblock-ad");
+roadblock_ad.style.display = 'none';
